44d0581c44c185372215d3a6f510c53a1e58bbaf
Diar Begisbayev <diar_beg64@mail.ru>
2023-10-03 22:06:10 +0600

    Updated mongourl

--- src/shared/config/database/mongodb.config.ts
+++ src/shared/config/database/mongodb.config.ts
@@ -9,7 +9,9 @@ export interface IMongoDBConfig {
 }
 
 export const InitDatabaseConfig = (mongoDbConfig: IMongoDBConfig) => {
-  const url = `mongodb+srv://${mongoDbConfig.username}:${mongoDbConfig.password}@${mongoDbConfig.cluster}.${mongoDbConfig.key}.mongodb.net/?retryWrites=true&w=majority`;
+  const url =
+    'mongodb+srv://victu-admin:123@victu-prod.i3lmgsp.mongodb.net/?retryWrites=true&w=majority';
+  // const url = `mongodb+srv://${mongoDbConfig.username}:${mongoDbConfig.password}@${mongoDbConfig.cluster}.${mongoDbConfig.key}.mongodb.net/?retryWrites=true&w=majority`;
 
   return MongooseModule.forRoot(url, { dbName: mongoDbConfig.database });
 };
